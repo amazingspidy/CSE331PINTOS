99c347c340c024d11f20cabc87de4041fa21c219
hyungminkim <2018ds21112@gmail.com>
2024-05-01 21:34:53 +0900

    until priority schedule

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -202,7 +202,17 @@ thread_create (const char *name, int priority,
   sf->ebp = 0;
 
   /* Add to run queue. */
+  /* go to ready list*/
   thread_unblock (t);
+
+  if (!list_empty(&ready_list)) {
+    struct thread *cur = thread_current();
+    struct thread *high_priority_thread = list_entry(list_front(&ready_list), struct thread, elem);
+    if (high_priority_thread->priority > cur->priority) {
+      thread_yield();
+    }
+  }
+  
 
   return tid;
 }
@@ -213,6 +223,20 @@ thread_create (const char *name, int priority,
    This function must be called with interrupts turned off.  It
    is usually a better idea to use one of the synchronization
    primitives in synch.h. */
+
+bool compare_by_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) {
+  if (a == NULL) {
+    return true;
+  }
+  if (b == NULL) {
+    return false;
+  }
+
+  struct thread *thread_a = list_entry(a, struct thread, elem);
+  struct thread *thread_b = list_entry(b, struct thread, elem);
+  return thread_a->priority > thread_b->priority;
+}
+
 void
 thread_block (void) 
 {
@@ -240,7 +264,8 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  //기존//list_push_back (&ready_list, &t->elem);
+  list_insert_ordered(&ready_list, &t->elem, compare_by_priority, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -310,8 +335,10 @@ thread_yield (void)
   ASSERT (!intr_context ());
 
   old_level = intr_disable ();
-  if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+  if (cur != idle_thread) {
+    //list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered(&ready_list, &cur->elem, compare_by_priority, NULL);
+  }
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -380,6 +407,14 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  list_sort(&ready_list, compare_by_priority, NULL);
+  if (!list_empty(&ready_list)) {
+    struct thread *cur = thread_current();
+    struct thread *high_priority_thread = list_entry(list_front(&ready_list), struct thread, elem);
+    if (high_priority_thread->priority > cur->priority) {
+      thread_yield();
+    }
+  }
 }
 
 /* Returns the current thread's priority. */
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -118,6 +118,7 @@ typedef void thread_func (void *aux);
 tid_t thread_create (const char *name, int priority, thread_func *, void *);
 
 void thread_block (void);
+bool compare_by_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 void thread_unblock (struct thread *);
 
 struct thread *thread_current (void);
